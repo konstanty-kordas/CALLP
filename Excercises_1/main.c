88136344d8a2ab850c740c547cf627a496fc76b0
konstanty-kordas <konstekkor@gmail.com>
2022-03-07 12:58:24 +0100

    redid as functions

--- Excercises_1/main.c
+++ Excercises_1/main.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
-int powerCheck(int a, int b){
+int power_check(int a, int b){
     if (a==1){
         return 1;
     }
     if (a%b!=0){
         return 0;
     }
-    return powerCheck(a/b,b);
+    return power_check(a/b,b);
 }
-
-int main(){
-    //1
-    printf("\n\n\n1.");
+void task_1(){
+    printf("1.");
     char name[11];
     printf("Input your name: ");
     scanf("%s", name);
     printf("Hello %s!\n", name);
-    //2
-    printf("\n\n\n2.");
+}
+void task_2() {
+    printf("2.");
     int value;
     printf("Input an integer (2): ");
     scanf("%d", &value);
-    
     if (value < 0) {
         value*=-1;
     }   
     printf("The absolute value is: %d\n",value); 
-    //3
-    printf("\n\n\n3.");
+    
+}
+void task_3() {
+    printf("3.");
     printf("Input an integer (3): ");
     int n;
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         printf("Hello World! \n"); 
     }
-    //4
-    printf("\n\n\n4.");
+}
+void task_4() {
+    int n;
+    printf("4.");
     printf("Input an integer (4): ");    
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
@@ -46,8 +48,9 @@ int main(){
         }
         printf("\n");
     }
-    //5
-    printf("\n\n\n5.");
+}
+void task_5() {
+    printf("5.");
     int hours, minutes, seconds;
     printf("Input a number of hours: ");
     scanf("%d", &hours);
@@ -55,40 +58,44 @@ int main(){
     scanf("%d", &minutes);
     seconds = minutes*60+hours*60*60;
     printf("The number of seconds is %d \n",seconds);
-    //6
-    printf("\n\n\n6.");
+}
+void task_6() {
+    printf("6.");
     float kilometers,miles;
     printf("Input a number of kilometers: ");
     scanf("%f", &kilometers);
     miles = kilometers*0.621371192;
     printf("Number of miles is %f \n",miles);
-    //7
-    printf("\n\n\n7.");
+}
+void task_7() {
+    float hours, minutes, seconds, kilometers;
+    printf("7.");
     printf("Input a number of hours: ");
-    scanf("%d", &hours);
+    scanf("%f", &hours);
     printf("Input a number of minutes: ");
-    scanf("%d", &minutes);
+    scanf("%f", &minutes);
     seconds = minutes*60+hours*60*60;
     printf("Input a number of kilometers: ");
     scanf("%f", &kilometers);
+    float pace,speed,miles;
     miles = kilometers*0.621371192;
-    float pace,speed;
     pace = seconds/miles;
-    float hours_decimal,minutes_decimal;
-    minutes_decimal  = minutes;
-    hours_decimal = hours + (minutes_decimal/60);
-    speed = kilometers/hours_decimal;
+    hours = hours + (minutes/60);
+    speed = kilometers/hours;
     printf("The speed is %f km/h \n",speed);
     printf("The pace is %f \n",pace);
-    //8
-    printf("\n\n\n8.");
+    
+}
+void task_8() {
+    printf("8.");
     float r,volume;
     printf("Input the radius of the sphere:");
     scanf("%f",&r);
     volume = (4.0/3.0) * M_PI * r*r*r;
     printf("The volume is %f \n", volume);
-    //9
-    printf("\n\n\n9.");
+}
+void task_9() {
+    printf("9.");
     int copies;
     float cost;
     printf("Input the number of copies: ");
@@ -101,8 +108,9 @@ int main(){
         cost+=5+(copies-1)*0.5;
     }
     printf("The cost is %f \n",cost);
-    //10
-    printf("\n\n\n10.");
+}
+void task_10() {
+    printf("10.");
     int a,b,c;
     printf("input a: ");
     scanf("%d",&a);
@@ -116,8 +124,9 @@ int main(){
     else {
         printf("No");
     }
-    //11
-    printf("\n\n\n11.");
+}
+void task_11() {
+    printf("11.");    
     char *palindrome;
     printf("Input a string: ");
     scanf("%ms",&palindrome);
@@ -126,32 +135,54 @@ int main(){
     while (palindrome[length]!=0){
         length+=1;
     }
-    int p = 1;
+    int bool = 1;
     printf("%d\n",length );
     for (int i=0;i<length;i++){
         if (palindrome[i]!=palindrome[length-1-i]){
-            p=0;
+            bool=0;
             break;
         }
     }
-    if (p){
+    if (bool){
         printf("This is a palindrome \n");
     }
     else {
         printf("This is not a palindrome \n");
     }
-    //12
-    printf("\n\n\n12.");
+}
+void task_12() {
+    int a,b;
+    printf("12.");
     printf("input a: ");
     scanf("%d",&a); 
     printf("input b: ");
     scanf("%d",&b);
-    if (powerCheck(a,b)){
-        printf("Yes, %d is a power of %d \n",a,b);
-    }
-    else {
+    if (a==0 || b==0){
         printf("No, %d is not a power of %d \n",a,b);
     }
+    else{
+        if (power_check(a,b)){
+            printf("Yes, %d is a power of %d \n",a,b);
+        }
+        else {
+            printf("No, %d is not a power of %d \n",a,b);
+        }
+    }
+}
+
+int main(){
+    task_1();
+    task_2();
+    task_3();
+    task_4();
+    task_5();
+    task_6();
+    task_7();
+    task_8();
+    task_9();
+    task_10();
+    task_11();
+    task_12();
     return 0; 
 }
 
