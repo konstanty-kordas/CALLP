15d30cd32a9e3e6945025573aa69a8af318276ac
konstanty-kordas <konstekkor@gmail.com>
2022-04-04 14:26:18 +0200

    5.0

--- Parser INI/main_45.c
+++ Parser INI/main_45.c
@@ -25,7 +25,6 @@ void insertFirst(char* name) {
     link->keys = NULL;
     link->NEXT = head;
     head = link;
-
 }
 void printList() { 
     struct section *ptr = head;
@@ -105,6 +104,52 @@ void insertKey(char*section_name,char*key_name,char*value) {
     parentSection->keys = link;
 }
 
+void split(char*input, char**arg1, char**arg2, char*delimeter){
+    int offset = 0;
+    while(input[offset] != '.'){
+        offset++;
+    }
+    int len = strlen(input);
+    
+    *arg1 = (char *)malloc(sizeof(char) * offset+2);
+    
+    *arg2 = (char *)malloc(sizeof(char) * len-offset+2);
+
+    strncpy(*arg1, input,offset);
+    arg1[offset+1] = '\0';
+    strncpy(*arg2,input+(offset+1),len-(offset+1));
+    arg2[len-(offset+1)] = '\0';
+}
+
+char* solve(char* section_name, char* key_name){
+    struct section* a;
+    a = findSection(section_name);
+    if (a!=NULL){   
+        struct key* b = findKey(section_name,key_name);
+        printf("FOUND: %s\n",a->name);
+        if (b!=NULL){
+            printf("KEY: %s \n",b->value);
+            return b->value;
+        }
+        else{
+            printf("Failed to find key [%s] in section {%s}\n",section_name,key_name);
+        }
+    }
+    else{
+        printf("Failed to find section [%s]\n", section_name);
+    }
+    return NULL;
+}
+
+int checkType(char*str){
+    int len = strlen(str);
+    for (int i=0;i<len;i++){
+        if (!isdigit(str[i])){
+            return 0;
+        }
+    }
+    return 1;
+}
 int main(int argc, char *argv[]){
     if (argc<3){
         printf("Not enough arguments given. Use command in format ./main_40 {$INI FILE TO PARSE} {$SECTION}.{$KEY}");
@@ -114,37 +159,48 @@ int main(int argc, char *argv[]){
     fname = argv[1];
     char* temp;
     temp = argv[2];
-    int offset = 0;
-    while(temp[offset] != '.'){
-        offset++;
-    }
-
-    int len = strlen(temp);
     char* section_name;
-    section_name = (char *)malloc(sizeof(char) * offset+2);
     char* key_name;
-    key_name = (char *)malloc(sizeof(char) * len-offset+2);
+    split(temp,&section_name,&key_name,".");
 
-    strncpy(section_name, temp,offset);
-    section_name[offset+1] = '\0';
-    strncpy(key_name,temp+(offset+1),len-(offset+1));
-    key_name[len-(offset+1)] = '\0';
 
-    printf("SECTION NAME %s, KEY NAME %s \n", section_name, key_name);
+    // printf("SECTION NAME %s, KEY NAME %s \n", section_name, key_name);
     FILE* fp = fopen(fname, "r");
 
     if(!fp) {
         perror("File opening failed");
         return EXIT_FAILURE;
     }
-    char line[100];
-    char* current_section = (char *)malloc(sizeof(char) * 60);
-
-    while (fgets(line, sizeof line, fp) != NULL){
+    char* line;
+    int ch;
+    char* current_section;
+    int pos;
+    int size = 0;
+    int file_size;
+    fseek(fp, 0, SEEK_END);
+    file_size = ftell(fp);
+    rewind(fp);
+    // while (fgets(line, sizeof line, fp) != NULL){
+    do{
+        pos = ftell(fp);
+        while ((ch=fgetc(fp))!='\n'){
+            size++;
+            if (ch==-1){
+                break;
+            }
+        }
+        if (pos>=file_size){
+            break;
+        }
+        line = (char*)calloc(size+2,sizeof(char));
+        fseek(fp,pos,SEEK_SET);
+        fgets(line, size+2, fp);
         if(line[0]=='['){
+            current_section = (char*)calloc(sizeof(char), size+2);
             current_section = strdup(line+1);
             current_section[strlen(current_section)-1] = '\0'; // removes last character
             current_section[strlen(current_section)-1] = '\0'; 
+            
             if (checkString(current_section)==0){
                 printf("FOUND ILLEGAL ON %s", current_section);
                 return 0;
@@ -157,7 +213,7 @@ int main(int argc, char *argv[]){
                 continue;
             }
             int j = 0;
-            char* curr_key = (char *)calloc(1,sizeof(char) * 60);
+            char* curr_key = (char *)calloc(1,sizeof(char) * size);
             while(line[j]!=' '){
                 curr_key[j] = line[j];
                 j++;
@@ -167,26 +223,72 @@ int main(int argc, char *argv[]){
                 return 0;
             }
             j+=3;
-            char* curr_val = (char *)calloc(1,sizeof(char) * 60);
+            char* curr_val = (char *)calloc(1,sizeof(char) * size);
             curr_val = line+j;
+            curr_val[strlen(curr_val)-1] = '\0';
             insertKey(current_section,curr_key,curr_val);
             
         }
     }
-    
-    struct section* a;
-    a = findSection(section_name);
-    if (a!=NULL){   
-        struct key* b = findKey(section_name,key_name);
-        printf("FOUND: %s\n",a->name);
-        if (b!=NULL){
-            printf("KEY: %s \n",b->value);
+    while (line);
+    char* val = solve(section_name,key_name);
+    if (argc>3){
+        char * operation = strdup(argv[3]);
+        char* section2 = strdup(argv[4]);
+        // printf("operation: %s \n", operation);
+        // printf("section2: %s \n", section2);
+        char*section_name2;
+        char*key_name2;
+        split(section2,&section_name2,&key_name2,".");
+        printf("SECTION NAME %s, KEY NAME %s \n", section_name2, key_name2);
+        char* val2 = solve(section_name2,key_name2);
+        if (val==NULL || val2==NULL){
+            return 0;
+        }
+        int type1 = checkType(val);
+        int type2 = checkType(val2);
+        if (type1!=type2){
+            printf("Keys are of a diffrent type\n");
+            return 0;
+        }
+        if (type1==0 && type2==0){
+            if (strcmp(operation,"+")!=0){
+                printf("Cannot perform %s on strings\n", operation);
+                return 0;
+            }
+            else{
+                char*result = (char*)malloc(sizeof(char)*(strlen(val)+strlen(val2)+1));
+                result = strcat(result, val);
+                result = strcat(result, val2);
+                printf("Concatenation results in:%s\n",result);
+                return 0;
+            }
         }
         else{
-            printf("Failed to find key [%s] in section {%s}\n",section_name,key_name);
+            printf("BOTH INTS!\n");
+            int v1 = atoi(val);
+            int v2 = atoi(val2);
+            if (strcmp(operation,"+")==0){
+                int result = v1+v2;
+                printf("Sum is %d\n", result);
+                return 0;
+            }
+            if (strcmp(operation,"-")==0){
+                int result = v1-v2;
+                printf("Diffrence is %d\n", result);
+                return 0;
+            }
+            if (strcmp(operation,"*")==0){
+                int result = v1*v2;
+                printf("Product is %d\n", result);
+                return 0;
+            }
+            if (strcmp(operation,"/")==0){
+                int result = v1/v2;
+                printf("Quotient is %d\n", result);
+                return 0;
+            }
         }
-    }
-    else{
-        printf("Failed to find section [%s]", section_name);
+        
     }
 }
